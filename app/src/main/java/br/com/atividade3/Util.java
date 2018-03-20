1246309cf03cf2a7ba4cde5ec2db0596d649759a
arqdsis <arqdsis@moclii0309.animaedu.intranet>
2018-03-20 20:44:29 -0300

    aula 03

---
+++ app/src/main/java/br/com/atividade3/Util.java
@@ -0,0 +1,32 @@
+package br.com.atividade3;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import java.lang.reflect.Field;
+
+/**
+ *
+ * Created by Henrique Pucci
+ *
+ * **/
+
+
+
+public class Util {
+    public static Drawable getDrawable(Context context, String nome){
+
+        Class<?> c = R.drawable.class;
+        try {
+            Field idField = c.getDeclaredField(nome);
+            int id = idField.getInt(idField);
+            return context.getResources().getDrawable(id, null);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
